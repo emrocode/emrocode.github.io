e54c5484487980c6361f61145cb80c8bf5359c1b
emrocode <92260849+emrocode@users.noreply.github.com>
2024-09-07 16:55:58 -0400

    Remove dotenv load & use deno env

--- _config.ts
+++ _config.ts
@@ -1,20 +1,17 @@
 import lume from "lume/mod.ts";
 import plugins from "./plugins.ts";
-import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
 import { type Page } from "lume/types.ts";
-
-const env = await load();
 
 const site = lume({
   src: "./src",
   dest: "./dist",
-  location: new URL(env["BASE_URL"]),
+  location: new URL(Deno.env.get("BASE_URL")),
 });
 
 site
   .use(plugins())
   .copy("assets", ".")
-  .remoteFile(env["REMOTE_FILE_ID"], env["REMOTE_FILE_URL"])
+  .remoteFile(Deno.env.get("REMOTE_FILE_ID"), Deno.env.get("REMOTE_FILE_URL"))
   .process([".html"], (pages: Page) => {
     for (const page of pages) {
       const externalLinks = page.document.querySelectorAll('a[href^="http"], a[href^="mailto:"]');
