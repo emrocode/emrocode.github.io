a70640701ab644f2da9da6b66151f8dd1085d414
emrocode <92260849+emrocode@users.noreply.github.com>
2024-10-06 15:23:52 -0300

    Update _config

--- _config.ts
+++ _config.ts
@@ -1,22 +1,23 @@
 import "jsr:@std/dotenv/load";
 import lume from "lume/mod.ts";
 import plugins from "./plugins.ts";
-import { type Page } from "lume/types.ts";
+import { env } from "./utils.ts";
 
 const site = lume({
   src: "./src",
   dest: "./dist",
-  location: new URL(Deno.env.get("BASE_URL")),
+  location: new URL(env("BASE_URL")),
 });
 
 site
   .use(plugins())
   .copy("assets", ".")
-  .remoteFile(Deno.env.get("REMOTE_FILE_ID"), Deno.env.get("REMOTE_FILE_URL"))
-  .process([".html"], (pages: Page) => {
+  .remoteFile(env("REMOTE_FILE_ID"), env("REMOTE_FILE_URL"))
+  .process([".html"], (pages) => {
     for (const page of pages) {
-      const externalLinks = page.document.querySelectorAll('a[href^="http"], a[href^="mailto:"]');
-      externalLinks.forEach((link: HTMLAnchorElement) => {
+      const externalLinks = page?.document?.querySelectorAll('a[href^="http"], a[href^="mailto:"]');
+      externalLinks?.forEach((anchor: Element) => {
+        const link = anchor as HTMLAnchorElement;
         link.setAttribute("target", "_blank");
         link.setAttribute("rel", "noopener noreferrer");
       });
