f1578f97adf0d6524d5708fb99acf35fb254843c
emrocode <92260849+emrocode@users.noreply.github.com>
2024-10-06 15:26:54 -0300

    Add scrollreveal

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,22 @@
 import Darkify from "npm:darkify-js";
+import ScrollReveal from "npm:scrollreveal";
 
 const dMode = new Darkify("button#dTheme", {
   useColorScheme: ["#f7f4e8", "#23211a"],
 });
 
 console.table(dMode.options);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const knowledgeBase = document.querySelectorAll(".skill");
+  const sr = ScrollReveal();
+
+  knowledgeBase.forEach((card, i) => {
+    sr.reveal(card, {
+      origin: "bottom",
+      distance: "15px",
+      duration: 500,
+      delay: i * 200,
+    });
+  });
+});
